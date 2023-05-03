ba73733f4665e7324f1bd3f787879c56bdce9d47
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-05-03 23:23:06 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import formData from "express-form-data";
+
+const app = express();
+
+app.set("trust proxy", 1);
+
+app.use(cors());
+app.use(formData.parse({ maxFilesSize: 10000000 }));
+app.use(formData.format());
+app.use(formData.stream());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("i love php");
+});
+
+const port = parseInt(process.env.PORT || "3005");
+
+app.listen(port, () => console.log("Up"));
