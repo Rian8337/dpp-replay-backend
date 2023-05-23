f3485f7089f815104e14e3e56c405eec2492e9ad
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-05-23 19:42:07 +0800

    Change score ID param to lowercase

--- src/routes/persist-online-replay.ts
+++ src/routes/persist-online-replay.ts
@@ -3,12 +3,12 @@ import { persistOnlineReplay } from "../replaySavingManager";
 
 const router = Router();
 
-router.post<"/", unknown, unknown, { uid: string; scoreId: string }>(
+router.post<"/", unknown, unknown, { uid: string; scoreid: string }>(
     "/",
     async (req, res) => {
         const success = await persistOnlineReplay(
             parseInt(req.body.uid),
-            parseInt(req.body.scoreId)
+            parseInt(req.body.scoreid)
         );
 
         if (!success) {
